28b43a612c2b4d764fca1447050b52cfc190db79
Johannes Kamprad <archlinux@kamprad.net>
2024-02-02 16:31:12 +0100

    Update prepare-action.sh

--- prepare-action.sh
+++ prepare-action.sh
@@ -8,21 +8,21 @@ git clone https://github.com/killajoe/EndeavourOS-Community-hyprland-ISO.git
 
 # copy live session Wallpaper into ISO structure:
 #cp livewall.png EndeavourOS-Community-hyprland-ISO/airootfs/root/
-cp pacman.conf EndeavourOS-Community-hyprland-ISO/airootfs/etc/pacman.conf
-cp EndeavourOS-Community-hyprland-ISO/packages-AUR.x86_64 /packages-AUR.x86_64
+cp pacman.conf $BUILDDIR/EndeavourOS-Community-hyprland-ISO/airootfs/etc/pacman.conf
+cp $BUILDDIR/EndeavourOS-Community-hyprland-ISO/packages-AUR.x86_64 /packages-AUR.x86_64
 
 # run preperations inside ISO structure
-cd EndeavourOS-Community-hyprland-ISO
+cd $BUILDDIR/EndeavourOS-Community-hyprland-ISO
 
 # Copy packages from Build Calamares git packages into iso structure:
 #cp /home/build/packages/* airootfs/root/packages/
 
 # Get mirrorlist for offline installs
 mkdir "airootfs/etc/pacman.d"
-wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/mirrorlist"
+wget -qN --show-progress -P "$BUILDDIR/EndeavourOS-Community-hyprland-ISO/airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/mirrorlist"
 
 # Get wallpaper for installed system
-wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
+wget -qN --show-progress -P "$BUILDDIR/EndeavourOS-Community-hyprland-ISO/airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
 
 # Make sure build scripts are executable
 chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
@@ -43,9 +43,9 @@ chown -R build:build /home/build/packages
 sudo -u build yay -S --noconfirm wlogout
 #sudo -u build yay -S --noconfirm swww
 #sudo -u build yay -S --noconfirm networkmanager-dmenu-bluetoothfix-git
-cp /home/build/packages/* EndeavourOS-Community-hyprland-ISO/airootfs/root/packages
+sudo cp /home/build/packages/* $BUILDDIR/EndeavourOS-Community-hyprland-ISO/airootfs/root/packages
 
 # Build liveuser skel
-chown -R build:build EndeavourOS-Community-hyprland-ISO/airootfs/root/endeavouros-skel-liveuser
-cd EndeavourOS-Community-hyprland-ISO/airootfs/root/endeavouros-skel-liveuser
+sudo chown -R build:build $BUILDDIR/EndeavourOS-Community-hyprland-ISO/airootfs/root/endeavouros-skel-liveuser
+cd $BUILDDIR/EndeavourOS-Community-hyprland-ISO/airootfs/root/endeavouros-skel-liveuser
 sudo -u build makepkg -f
