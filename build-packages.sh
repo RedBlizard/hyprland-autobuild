09324a509ecc55238ab386ff148ba2177e8a5132
Johannes Kamprad <archlinux@kamprad.net>
2024-02-02 12:13:08 +0100

    Update build-packages.sh

--- build-packages.sh
+++ build-packages.sh
@@ -6,7 +6,5 @@ mkdir /usr/share/packages
 pacman -Syy
 pacman -S --noconfirm --needed git yay
 useradd -m -G wheel -s /bin/bash build
-mkdir -p /home/build/packages
-chown -R build:build /home/build/packages
-sudo -u  build yay -S --noconfirm - < EndeavourOS-Community-hyprland-ISO/packages-AUR.x86_64
+sudo -u  build yay -S --noconfirm - < /packages-AUR.x86_64
 cp /usr/share/packages/* airootfs/root/packages/
