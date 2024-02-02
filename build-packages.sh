a4e4206022c859663e88f1c381110b68b830d61e
Johannes Kamprad <archlinux@kamprad.net>
2024-02-02 14:54:39 +0100

    Update build-packages.sh

--- build-packages.sh
+++ build-packages.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # building AUR packages and make sur esaving them into airootfs/root/packages:
 echo "%wheel         ALL = (ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo PKGDEST=/usr/share/packages >> /etc/makepkg.conf
-mkdir /usr/share/packages
+
 pacman -Syy
 pacman -S --noconfirm --needed git yay
 useradd -m -G wheel -s /bin/bash build
 
+echo PKGDEST=/home/build/packages >> /etc/makepkg.conf
+mkdir /home/build/packages
+chown -R build:buil /home/build/packages
 # Build liveuser skel
 #get_pkg() {
 #    pacman -Syw $1 --noconfirm --cachedir EndeavourOS-Community-hyprland-ISO/airootfs/root/packages
@@ -20,4 +22,4 @@ cd EndeavourOS-Community-hyprland-ISO/airootfs/root/endeavouros-skel-liveuser
 sudo -u build makepkg -f
 cd /
 sudo -u  build yay -S --noconfirm - < /packages-AUR.x86_64
-cp /usr/share/packages/* EndeavourOS-Community-hyprland-ISO/airootfs/root/packages/
+cp /home/build/packages/* EndeavourOS-Community-hyprland-ISO/airootfs/root/packages/
