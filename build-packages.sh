d1347a1e982355d596f46443fd06f6d77bd6c623
Johannes Kamprad <archlinux@kamprad.net>
2024-02-02 11:54:36 +0100

    Update build-packages.sh

--- build-packages.sh
+++ build-packages.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-# building calamares git packages:
+# building AUR packages and make sur esaving them into airootfs/root/packages:
 echo "%wheel         ALL = (ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-#pacman -Syy
-#pacman -S --noconfirm --needed git cmake extra-cmake-modules kpmcore boost python-jsonschema python-pyaml python-unidecode gawk qt5-svg qt5-webengine yaml-cpp networkmanager upower kcoreaddons5 kconfig5 ki18n5 kservice5 kwidgetsaddons5 kpmcore squashfs-tools rsync cryptsetup qt5-xmlpatterns doxygen dmidecode gptfdisk hwinfo kparts5 polkit-qt5 python solid5 qt5-tools boost-libs libpwquality ckbcomp qt5-quickcontrols2
+echo PKGDEST=/usr/share/packages >> /etc/makepkg.conf
+mkdir /usr/share/packages
+pacman -Syy
+pacman -S --noconfirm --needed git yay
 useradd -m -G wheel -s /bin/bash build
-#cd /home/build
-#mkdir -p /home/build/packages
-#chown -R build:build /home/build/packages
-#sudo -u  build git clone https://github.com/endeavouros-team/PKGBUILDS
-#cd /home/build/PKGBUILDS/calamares-git
-#sudo -u  build makepkg -f
-#cp *.pkg.tar.zst /home/build/packages/
+mkdir -p /home/build/packages
+chown -R build:build /home/build/packages
+sudo -u  build yay -S --noconfirm - < EndeavourOS-Community-hyprland-ISO/packages-AUR.x86_64
+cp /usr/share/packages/* airootfs/root/packages/
